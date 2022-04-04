62131326dbe2e3162f64008fcbc92c27e032ce29
Edward Ramírez <edal_ramirez@hotmail.com>
2022-04-03 22:52:10 -0500

    feat: add custom endpoint to fetch and create data from jokes api

--- backend/src/api/question/controllers/question.js
+++ backend/src/api/question/controllers/question.js
@@ -3,7 +3,18 @@
 /**
  *  question controller
  */
+const axios = require('axios');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::question.question');
+module.exports = createCoreController('api::question.question', () => ({
+  async fetchSampleData(ctx) {
+    const response = await axios.get('https://v2.jokeapi.dev/joke/Any?type=twopart');
+
+    const { setup, delivery } = response.data;
+    ctx.request.body = {data: {title: setup, text: delivery}}
+
+    const data =  await strapi.controller('api::question.question').create(ctx);
+    return data;
+  },
+}));
