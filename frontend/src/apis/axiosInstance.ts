11f8096a2bdae6cc3f77ee6f3cfd6ccad4c09265
Edward Ramírez <edal_ramirez@hotmail.com>
2022-04-04 00:13:48 -0500

    fix: change axios instance baseUrl to the production one

--- frontend/src/apis/axiosInstance.ts
+++ frontend/src/apis/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:1337/api',
+  baseURL: 'https://strapi-faq.herokuapp.com/api',
 });
 
 export default axiosInstance;
